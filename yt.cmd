ea21793a7bb416743d5c28a4d0530bab5123c94c
alex <alex512@o2.pl>
2020-03-22 12:00:34 +0100

    get parameters

--- yt.cmd
+++ yt.cmd
@@ -1,10 +1,13 @@
 @echo off
 
-cd
-echo %~dp0
 cd /d "%~dp0"
-cd
-bash -c './yt.sh "%1" %2 %3'
 
-pause
+rem echo "%~nx1"
+copy %1 . > nul:
+set f=%~nx1
+rem echo "%f%"
+bash -c './yt.sh "%f%" %2 %3'
+rem bash -c './yt.sh "%1" %2 %3'
+
+rem pause
  
